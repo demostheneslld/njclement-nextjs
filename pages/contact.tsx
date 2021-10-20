c03bf4fb5b360aa5e67d9cb05562c5df1ced1a71
Nathan Clement <nathan.clement@printforia.com>
2021-10-20 08:38:06 -0700

    No new work message

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -7,6 +7,7 @@ const Portfolio = (): ReactElement => {
       <Head>
         <title>Contact | Nathaniel J. Clement</title>
       </Head>
+      <div><i>I am not currently accepting new work in order to reserve capacity for my full time employer.</i></div>
       <div><b>Email:</b> <a className='text-accent-800' href='mailto: inquiries@njclement.com'>inquiries@njclement.com</a></div>
     </div>
   )
