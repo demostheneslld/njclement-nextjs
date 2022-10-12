0bc09debc0d90d24f63e85fb48b6f23e498bfc8b
Nathan Clement <nathan.clement.j@gmail.com>
2022-10-12 09:49:23 -0700

    Buildkit

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -1,4 +1,4 @@
 # Build Docker Image
 echo 'Starting Docker Build'
 echo 'Git Branch ' $1
-docker build --pull --rm -f "Dockerfile" -t ghcr.io/demostheneslld/njclement-nextjs:$1 "."
+DOCKER_BUILDKIT=1 docker build --pull --rm -f "Dockerfile" -t ghcr.io/demostheneslld/njclement-nextjs:$1 "."
