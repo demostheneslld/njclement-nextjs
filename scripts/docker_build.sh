a3b69ac17b66feb4bc5c56c1911206ef0c9cc40d
Nathan Clement <nathan.clement.j@gmail.com>
2022-11-18 15:20:01 -0800

    Valid Tag Names

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -1,4 +1,7 @@
 # Build Docker Image
 echo 'Starting Docker Build'
 echo 'Git Branch ' $1
-DOCKER_BUILDKIT=1 docker build --pull --rm -f "Dockerfile" -t ghcr.io/demostheneslld/njclement-nextjs:$1 "."
+replace='deploy\/'
+replace_with=""
+deploy_tag=$(echo $1 | sed "s/$replace/$replace_with/g")
+DOCKER_BUILDKIT=1 docker build --pull --rm -f "Dockerfile" -t ghcr.io/demostheneslld/njclement-nextjs:$deploy_tag "."
