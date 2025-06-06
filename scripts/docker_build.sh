f573fa798751f5d1e2a094c232e466dfb25137e8
Nathaniel Clement <8669241+demostheneslld@users.noreply.github.com>
2025-06-06 08:31:33 -0700

    Fix Docker workflows and scripts

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
 # Build Docker Image
+set -euo pipefail
+
 echo 'Starting Docker Build'
-echo 'Git Branch ' $1
-replace='deploy\/'
-replace_with=""
-deploy_tag=$(echo $1 | sed "s/$replace/$replace_with/g")
-DOCKER_BUILDKIT=1 docker build --pull --rm -f "Dockerfile" -t ghcr.io/demostheneslld/njclement-nextjs:$deploy_tag "."
+ref="$1"
+echo 'Git Branch/Ref ' "$ref"
+
+tag=${ref//\//-}
+DOCKER_BUILDKIT=1 docker build --pull --rm -f Dockerfile -t ghcr.io/demostheneslld/njclement-nextjs:"$tag" .
--- scripts/docker_local.sh
+++ scripts/docker_local.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+set -euo pipefail
+
 echo 'Starting Docker Build and Run Local'
 tag='njclement-nextjs-local:latest'
-DOCKER_BUILDKIT=1 docker build --pull --rm -f "Dockerfile" -t $tag "."
+DOCKER_BUILDKIT=1 docker build --pull --rm -f Dockerfile -t "$tag" .
 echo 'Build Complete'
+
 echo 'Running Container'
-docker run -p 3000:3000 $tag
+docker run -p 3000:3000 "$tag"
